84790153c77d3bf03b59726c6f37e9fb3eebbb8d
mmd-osm <mmd.osm@gmail.com>
2017-07-16 10:42:19 +0200

    changed: don't overwrite since and until with zero in special case This screwed up get_since and get_until Fixes https://github.com/drolbr/Overpass-API/issues/412

--- src/overpass_api/statements/changed.cc
+++ src/overpass_api/statements/changed.cc
@@ -225,11 +225,13 @@ Changed_Statement::Changed_Statement
     add_static_error(temp.str());
   }
 
-  since = Timestamp(attributes["since"]).timestamp;
+  if (attributes["since"] != "auto" && attributes["since"] != "init")
+    since = Timestamp(attributes["since"]).timestamp;
   if (!behave_trivial && attributes["since"] != "auto" && (since == 0 || since == NOW))
     add_static_error("The attribute \"since\" must contain a timestamp exactly in the form \"yyyy-mm-ddThh:mm:ssZ\".");
 
-  until = Timestamp(attributes["until"]).timestamp;
+  if (attributes["until"] != "auto")
+    until = Timestamp(attributes["until"]).timestamp;
   if (!behave_trivial && attributes["until"] != "auto" && (until == 0 || until == NOW))
     add_static_error("The attribute \"until\" must contain a timestamp exactly in the form \"yyyy-mm-ddThh:mm:ssZ\".");
 }
