mmd-osm/Overpass-API

commit ec732ced276c955bc7f621c8294f8ca3d325335b

Fixed a bug: user criterion did not work for type nwr, because it was considered standalone.

src/overpass_api/statements/user.h
@@ -46,7 +46,7 @@ class User_Statement : public Output_Statement
 
     struct Criterion_Maker : public Statement::Criterion_Maker
     {
-      virtual bool can_standalone(const std::string& type) { return true; }
+      virtual bool can_standalone(const std::string& type) { return false; }
       virtual Statement* create_criterion(const Token_Node_Ptr& tree_it,
           const std::string& type, const std::string& into,
           Statement::Factory& stmt_factory, Parsed_Query& global_settings, Error_Output* error_output);

osm-3s_testing/expected/osm3s_query_87/xml.out.log
@@ -1,3 +1,5 @@
 <osm-script>
-  <user type="node" name="A user name @&lt;&gt;"/>
+  <query into="_" type="node">
+    <user type="node" name="A user name @&lt;&gt;"/>
+  </query>
 </osm-script>

osm-3s_testing/expected/osm3s_query_88/xml.out.log
@@ -1,3 +1,5 @@
 <osm-script>
-  <user type="node" name="Username" into="out"/>
+  <query into="out" type="node">
+    <user type="node" name="Username" into="out"/>
+  </query>
 </osm-script>

osm-3s_testing/expected/osm3s_query_89/xml.out.log
@@ -1,3 +1,5 @@
 <osm-script>
-  <user type="node" uid="496"/>
+  <query into="_" type="node">
+    <user type="node" uid="496"/>
+  </query>
 </osm-script>

osm-3s_testing/expected/osm3s_query_90/xml.out.log
@@ -1,3 +1,5 @@
 <osm-script>
-  <user type="node" uid="496" into="out"/>
+  <query into="out" type="node">
+    <user type="node" uid="496" into="out"/>
+  </query>
 </osm-script>

src/overpass_api/statements/id_query.h
@@ -43,7 +43,7 @@ class Id_Query_Statement : public Output_Statement
 
     struct Criterion_Maker : public Statement::Criterion_Maker
     {
-      virtual bool can_standalone(const std::string& type) { return true; }
+      virtual bool can_standalone(const std::string& type) { return type != "nwr"; }
       virtual Statement* create_criterion(const Token_Node_Ptr& tree_it,
           const std::string& type, const std::string& into,
           Statement::Factory& stmt_factory, Parsed_Query& global_settings, Error_Output* error_output);

commit b450ceb6df8a27b31ac5fb7b87f822a6c9f3e9af

(user: ) - fix perf regression in ec732ced276c955bc7f621c8294f8ca3d325335b See https://github.com/drolbr/Overpass-API/issues/467#issuecomment-381605718

src/overpass_api/statements/user.cc
@@ -345,7 +345,7 @@ void User_Statement::execute(Resource_Manager& rman)
   if (rman.get_desired_timestamp() == NOW)
   {
 
-    if ((result_type == "") || (result_type == "node"))
+    if ((result_type == "") || (result_type == "node") || (result_type == "nwr"))
     {
       std::set< std::pair< Uint32_Index, Uint32_Index > > ranges;
       constraint.get_ranges(rman, ranges);
@@ -363,7 +363,7 @@ void User_Statement::execute(Resource_Manager& rman)
           });
     }
 
-    if ((result_type == "") || (result_type == "way"))
+    if ((result_type == "") || (result_type == "way") || (result_type == "nwr"))
     {
       std::set< std::pair< Uint31_Index, Uint31_Index > > ranges;
       constraint.get_ranges(rman, ranges);
@@ -382,7 +382,7 @@ void User_Statement::execute(Resource_Manager& rman)
 
     }
 
-    if ((result_type == "") || (result_type == "relation"))
+    if ((result_type == "") || (result_type == "relation") || (result_type == "nwr"))
     {
       std::set< std::pair< Uint31_Index, Uint31_Index > > ranges;
       constraint.get_ranges(rman, ranges);
@@ -403,7 +403,7 @@ void User_Statement::execute(Resource_Manager& rman)
   else
   {
 
-    if ((result_type == "") || (result_type == "node"))
+    if ((result_type == "") || (result_type == "node") || (result_type == "nwr"))
     {
       std::set< std::pair< Uint32_Index, Uint32_Index > > ranges;
       constraint.get_ranges(rman, ranges);
@@ -414,7 +414,7 @@ void User_Statement::execute(Resource_Manager& rman)
       filter_attic_elements(rman, rman.get_desired_timestamp(), into.nodes, into.attic_nodes);
     }
 
-    if ((result_type == "") || (result_type == "way"))
+    if ((result_type == "") || (result_type == "way") || (result_type == "nwr"))
     {
       std::set< std::pair< Uint31_Index, Uint31_Index > > ranges;
       constraint.get_ranges(rman, ranges);
@@ -425,7 +425,7 @@ void User_Statement::execute(Resource_Manager& rman)
       filter_attic_elements(rman, rman.get_desired_timestamp(), into.ways, into.attic_ways);
     }
 
-    if ((result_type == "") || (result_type == "relation"))
+    if ((result_type == "") || (result_type == "relation") || (result_type == "nwr"))
     {
       std::set< std::pair< Uint31_Index, Uint31_Index > > ranges;
       constraint.get_ranges(rman, ranges);

src/overpass_api/statements/user.h
@@ -46,7 +46,7 @@ class User_Statement : public Output_Statement
 
     struct Criterion_Maker : public Statement::Criterion_Maker
     {
-      virtual bool can_standalone(const std::string& type) { return false; }
+      virtual bool can_standalone(const std::string& type) { return true; }
       virtual Statement* create_criterion(const Token_Node_Ptr& tree_it,
           const std::string& type, const std::string& into,
           Statement::Factory& stmt_factory, Parsed_Query& global_settings, Error_Output* error_output);