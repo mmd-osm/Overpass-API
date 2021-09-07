984f15af6a5564ad09bc69def2ff684ef60bdf12
mmd-osm <mmd.osm@gmail.com>
2021-09-07 20:02:41 +0200

    Remove remaining template script

--- src/test-bin/run_testsuite_interpreter.sh
+++ src/test-bin/run_testsuite_interpreter.sh
@@ -194,8 +194,6 @@ rm -fR input/interpreter_*
 # Prepare testing the statements with meta
 mkdir -p input/update_database/
 rm -f input/update_database/*
-mkdir -p input/update_database/templates/
-cp -p $BASEDIR/templates/* input/update_database/templates/
 $BASEDIR/test-bin/generate_test_file_meta $DATA_SIZE >input/update_database/stdin.log
 $BASEDIR/bin/update_database --db-dir=input/update_database/ --meta --version=mock-up-init <input/update_database/stdin.log
 
