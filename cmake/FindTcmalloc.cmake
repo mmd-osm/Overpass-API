e002a4693731be726ee59a97e30cb22c1d80ea5c
mmd-osm <mmd.osm@gmail.com>
2022-01-11 18:05:45 +0100

    updated cmake script

--- cmake/FindTcmalloc.cmake
+++ cmake/FindTcmalloc.cmake
@@ -5,43 +5,49 @@
 #  Tcmalloc_LIBRARIES   - List of libraries when using Tcmalloc.
 #  Tcmalloc_FOUND       - True if Tcmalloc found.
 
-find_path(Tcmalloc_INCLUDE_DIR google/tcmalloc.h NO_DEFAULT_PATH PATHS
-  ${HT_DEPENDENCY_INCLUDE_DIR}
-  /usr/include
-  /opt/local/include
-  /usr/local/include
-)
 
-if (USE_TCMALLOC)
-  set(Tcmalloc_NAMES tcmalloc)
-else ()
-  set(Tcmalloc_NAMES tcmalloc_minimal tcmalloc)
+if (Tcmalloc_INCLUDE_DIR)
+    # Already in cache, be silent
+    set(Tcmalloc_FIND_QUIETLY TRUE)
 endif ()
 
-find_library(Tcmalloc_LIBRARY NO_DEFAULT_PATH
-  NAMES ${Tcmalloc_NAMES}
-  PATHS ${HT_DEPENDENCY_LIB_DIR} /lib /usr/lib /usr/local/lib /opt/local/lib
-)
+if(DEFINED GPERFTOOLS_ROOT)
+    message(STATUS "GPERFTOOLS_ROOT: ${GPERFTOOLS_ROOT}")
+    find_path(Tcmalloc_INCLUDE_DIR google/heap-checker.h ${GPERFTOOLS_ROOT}/include)
+    find_library(Tcmalloc_LIBRARY NAME tcmalloc PATHS ${GPERFTOOLS_ROOT}/lib NO_DEFAULT_PATH)
+else()
+    find_path(Tcmalloc_INCLUDE_DIR google/heap-checker.h
+        /opt/local/include
+        /usr/local/include
+        /usr/include
+    )
+    set(TMP_PATH $ENV{LD_LIBRARY_PATH})
+    if ($TMP_PATH)
+        string(REPLACE ":" " " LD_LIBRARY_PATH_STR $TMP_PATH)
+    endif()
+    find_library(Tcmalloc_LIBRARY NAME tcmalloc
+        PATHS ${LD_LIBRARY_PATH_STR} /usr/lib /usr/local/lib /opt/local/lib
+    )
+endif(DEFINED GPERFTOOLS_ROOT)
 
 if (Tcmalloc_INCLUDE_DIR AND Tcmalloc_LIBRARY)
-  set(Tcmalloc_FOUND TRUE)
-  set( Tcmalloc_LIBRARIES ${Tcmalloc_LIBRARY} )
+    set(Tcmalloc_FOUND TRUE)
+    set(Tcmalloc_LIBRARIES ${Tcmalloc_LIBRARY})
 else ()
-  set(Tcmalloc_FOUND FALSE)
-  set( Tcmalloc_LIBRARIES )
+    set(Tcmalloc_FOUND FALSE)
+    set(Tcmalloc_LIBRARIES)
 endif ()
 
 if (Tcmalloc_FOUND)
-  message(STATUS "Found Tcmalloc: ${Tcmalloc_LIBRARY}")
+    if (NOT Tcmalloc_FIND_QUIETLY)
+        message(STATUS "Found Tcmalloc: ${Tcmalloc_LIBRARY}")
+    endif ()
 else ()
-  message(STATUS "Not Found Tcmalloc: ${Tcmalloc_LIBRARY}")
-  if (Tcmalloc_FIND_REQUIRED)
-    message(STATUS "Looked for Tcmalloc libraries named ${Tcmalloc_NAMES}.")
-    message(FATAL_ERROR "Could NOT find Tcmalloc library")
-  endif ()
+    message(STATUS "Not Found Tcmalloc: ${Tcmalloc_LIBRARY}")
+    if (Tcmalloc_FIND_REQUIRED)
+        message(STATUS "Looked for Tcmalloc libraries named tcmalloc.")
+        message(FATAL_ERROR "Could NOT find Tcmalloc library")
+    endif ()
 endif ()
 
-mark_as_advanced(
-  Tcmalloc_LIBRARY
-  Tcmalloc_INCLUDE_DIR
-  )
+mark_as_advanced(Tcmalloc_LIBRARY Tcmalloc_INCLUDE_DIR)
