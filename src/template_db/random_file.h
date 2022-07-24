2b2630be4b60d1eb345ab2ffabbef583c0fd3e9e
mmd-osm <mmd.osm@gmail.com>
2022-07-24 15:14:26 +0200

    Fix performance regression
    clang++-13 generated fairly expensive code here

--- src/template_db/random_file.h
+++ src/template_db/random_file.h
@@ -163,8 +163,7 @@ void Random_File< Key, Value >::move_cache_window(uint32 pos)
   if ((index->get_blocks().size() <= pos) || (index->get_blocks()[pos].pos == index->npos))
   {
     // Reset the whole cache to zero.
-    for (uint32 i = 0; i < block_size * compression_factor; ++i)
-      cache[i] = 0;
+    zero_padding(cache.get(), block_size * compression_factor);
   }
   else
   {
